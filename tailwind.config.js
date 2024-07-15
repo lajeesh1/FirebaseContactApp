7cf7a56f74058bdaa6ad3e571d0bd242fdfb2dc2
Lajeesh <gokulam.9920@gmail.com>
2024-07-15 20:28:42 +0530

    home

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        lightYellow: '#FFEAAE',
+        white: '#FFFFFF',
+        black: '#000000',
+        orange: '#F6820C',
+        yellow: '#FCCA3F',
+        darkYellow: '#EEAB37',
+        lightOrange: '#F4BD62',
+        paleYellow: '#FDE068',
+        deepOrange: '#FFA50E',
+        brightOrange: '#FFA712',
+        paleOrange: '#FFC24A',
+        gray: '#323334',
+        offWhite: '#FDFDFD'
+      }
+    },
   },
   plugins: [],
 };
