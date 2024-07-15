331544116e93244b4832a76d78aab94a81a51283
lajeesh1 <gokulam.9920@gmail.com>
2024-07-15 22:16:13 +0530

    input field completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import { FaSearch } from "react-icons/fa";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="bg-red-600">Hello welcome dear</h1>
+    <div className="max-w-[350px] mx-auto ">
+      <Navbar />
+      <div className="input flex relative items-center m-3 ">
+        <FaSearch className="text-white text-2xl absolute ml-3 " />
+        <input
+          type="text"
+          className="flex-grow outline-none text-white bg-transparent border border-white rounded-md h-[40px] pl-[50px]"
+        />
+      </div>
     </div>
   );
 };
 
 export default App;
-
