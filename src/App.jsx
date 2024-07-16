ded156e337b752ad0034f9441cef29e8311c28fb
Lajeesh <gokulam.9920@gmail.com>
2024-07-16 12:53:47 +0530

    add button finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import Navbar from "./components/Navbar";
-import { FaSearch } from "react-icons/fa";
+import { CiSearch } from "react-icons/ci";
+import { FaPlusSquare } from "react-icons/fa";
 
 const App = () => {
   return (
     <div className="max-w-[350px] mx-auto ">
       <Navbar />
       <div className="input flex relative items-center m-3 ">
-        <FaSearch className="text-white text-2xl absolute ml-3 " />
+        <CiSearch className="font-thin text-white text-2xl absolute ml-3  " />
         <input
           type="text"
-          className="flex-grow outline-none text-white bg-transparent border border-white rounded-md h-[40px] pl-[50px]"
+          className=" flex-grow outline-none text-white bg-transparent border border-white rounded-md h-[40px] pl-[50px]"
         />
+        <FaPlusSquare className="cursor-pointer text-5xl text-white ml-1 bg" />
       </div>
     </div>
   );
